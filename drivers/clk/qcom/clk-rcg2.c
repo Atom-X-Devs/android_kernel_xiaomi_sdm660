0e2ef9790879e2ca019eeae66a829a68b0a1f8db
Divyanshu-Modi <divyan.m05@gmail.com>
2021-12-27 10:40:58 +0530

    clk: qcom: compile out clk-debug when !CONFIG_*_DEBUGCC_*
     Useless without debugcc enabled
    
    Co-authored-by: Tyler Nijmeh <tylernij@gmail.com>
    Co-authored-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/clk/qcom/clk-rcg2.c
+++ drivers/clk/qcom/clk-rcg2.c
@@ -812,7 +812,9 @@ const struct clk_ops clk_rcg2_ops = {
 	.set_rate_and_parent = clk_rcg2_set_rate_and_parent,
 	.list_rate = clk_rcg2_list_rate,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL_GPL(clk_rcg2_ops);
 
@@ -826,7 +828,9 @@ const struct clk_ops clk_rcg2_floor_ops = {
 	.set_rate_and_parent = clk_rcg2_set_floor_rate_and_parent,
 	.list_rate = clk_rcg2_list_rate,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL_GPL(clk_rcg2_floor_ops);
 
@@ -957,7 +961,9 @@ const struct clk_ops clk_edp_pixel_ops = {
 	.set_rate_and_parent = clk_edp_pixel_set_rate_and_parent,
 	.determine_rate = clk_edp_pixel_determine_rate,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL_GPL(clk_edp_pixel_ops);
 
@@ -1019,7 +1025,9 @@ const struct clk_ops clk_byte_ops = {
 	.set_rate_and_parent = clk_byte_set_rate_and_parent,
 	.determine_rate = clk_byte_determine_rate,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL_GPL(clk_byte_ops);
 
@@ -1093,7 +1101,9 @@ const struct clk_ops clk_byte2_ops = {
 	.set_rate_and_parent = clk_byte2_set_rate_and_parent,
 	.determine_rate = clk_byte2_determine_rate,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL_GPL(clk_byte2_ops);
 
@@ -1187,7 +1197,9 @@ const struct clk_ops clk_pixel_ops = {
 	.set_rate_and_parent = clk_pixel_set_rate_and_parent,
 	.determine_rate = clk_pixel_determine_rate,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL_GPL(clk_pixel_ops);
 
@@ -1271,7 +1283,9 @@ const struct clk_ops clk_dp_ops = {
 	.set_rate_and_parent = clk_dp_set_rate_and_parent,
 	.determine_rate = clk_dp_determine_rate,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL_GPL(clk_dp_ops);
 
@@ -1367,7 +1381,9 @@ const struct clk_ops clk_gfx3d_ops = {
 	.set_rate_and_parent = clk_gfx3d_set_rate_and_parent,
 	.determine_rate = clk_gfx3d_determine_rate,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL_GPL(clk_gfx3d_ops);
 
@@ -1581,7 +1597,9 @@ const struct clk_ops clk_rcg2_shared_ops = {
 	.set_rate = clk_rcg2_shared_set_rate,
 	.set_rate_and_parent = clk_rcg2_shared_set_rate_and_parent,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL_GPL(clk_rcg2_shared_ops);
 
@@ -1879,7 +1897,9 @@ const struct clk_ops clk_rcg2_dependent_ops = {
 	.set_rate_and_parent = clk_rcg2_dependent_set_rate_and_parent,
 	.list_rate = clk_rcg2_list_rate,
 	.list_registers = clk_rcg2_list_registers,
+#ifdef CONFIG_QCOM_CLK_DEBUG
 	.bus_vote = clk_debug_bus_vote,
+#endif
 };
 EXPORT_SYMBOL(clk_rcg2_dependent_ops);
 
